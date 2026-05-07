807fd884056485ab40c608e9ae3df6c4ecafcac1
WangJinyi <44677889+gslkj3@users.noreply.github.com>
2026-05-07 15:21:58 +0800

    performance Optimization
    Adopting the idea of loop unrolling, we manually break through the black-box boundary between Python compilation and Triton compilation. This reduces the number of launched programs while increasing the data processing volume of each individual program. Performance is improved in three aspects: launch overhead, hardware utilization, and the efficiency of contiguous memory data transfer.
    
    Signed-off-by: WangJinyi <44677889+gslkj3@users.noreply.github.com>

--- src/flag_gems/ops/per_token_group_quant_fp8.py
+++ src/flag_gems/ops/per_token_group_quant_fp8.py
@@ -13,11 +13,10 @@ if torch_device_fn.is_available() and get_device_capability() >= (9, 0):
 else:
     SUPPORTED_FP8_DTYPE = torch.float32
 
-
 logger = logging.getLogger(__name__)
 
-
 @triton.jit
+
 def _per_token_group_quant_fp8(
     y_ptr,
     y_q_ptr,
@@ -30,33 +29,45 @@ def _per_token_group_quant_fp8(
     fp8_max,
     scale_ue8m0,
     BLOCK: tl.constexpr,
+    groups_per_program: tl.constexpr,
 ):
     groups_per_row = y_num_columns // group_size
+    pid = tl.program_id(0)
+  
+    pairs_per_row = groups_per_row // groups_per_program
+    row = pid // pairs_per_row
+    pair_id = pid % pairs_per_row
 
-    g_id = tl.program_id(0)
-    row = g_id // groups_per_row
-    row_g_id = g_id % groups_per_row
-
-    y_ptr += (row * y_row_stride) + (row_g_id * group_size)
-    y_q_ptr += g_id * group_size
-    y_s_ptr += g_id
-
+    group0 = pair_id * groups_per_program
+    g0 = row * groups_per_row + group0
+    base = y_ptr + row * y_row_stride
     cols = tl.arange(0, BLOCK)
     mask = cols < group_size
 
-    y = tl.load(y_ptr + cols, mask=mask, other=0.0).to(tl.float32)
-    _absmax = tl.maximum(tl.max(tl.abs(y)), eps)
-    y_s = _absmax / fp8_max
+    for i in tl.static_range(0,groups_per_program):
+            
+        group = group0 + i
+        g = g0 + i
+        y_ptr_g = base + group * group_size
+        y_q_ptr_g = y_q_ptr + g * group_size
+        y_s_ptr_g = y_s_ptr + g
+        y = tl.load(y_ptr_g + cols, mask=mask, other=0.0).to(tl.float32)
 
-    if scale_ue8m0:
-        y_s = tl.exp2(tl.ceil(tl.log2(tl.maximum(tl.abs(y_s), 1e-10))))
+        abs_g = tl.abs(y)
+        max_g = tl.max(abs_g)
+        
+        y_s = tl.maximum(max_g, eps) / fp8_max
 
-    y_q = tl.clamp(y / y_s, fp8_min, fp8_max).to(y_q_ptr.dtype.element_ty)
-
-    tl.store(y_q_ptr + cols, y_q, mask=mask)
-    tl.store(y_s_ptr, y_s)
-
-
+        if scale_ue8m0:
+            y_s = tl.exp2(tl.ceil(tl.log2(tl.maximum(tl.abs(y_s), 1e-10))))
+          
+        y_q = tl.clamp(y / y_s, fp8_min, fp8_max).to(y_q_ptr.dtype.element_ty)
+    
+        tl.store(y_q_ptr_g + cols, y_q, mask=mask)
+        tl.store(y_s_ptr_g, y_s)
+        
+    
+#kernel优化版本M/8
 @triton.jit
 def _per_token_group_quant_fp8_colmajor(
     y_ptr,
@@ -71,32 +82,43 @@ def _per_token_group_quant_fp8_colmajor(
     fp8_max,
     scale_ue8m0,
     BLOCK: tl.constexpr,
+    groups_per_program: tl.constexpr,
 ):
     groups_per_row = y_num_columns // group_size
+    pid = tl.program_id(0)
+  
+    pairs_per_row = groups_per_row // groups_per_program
+    row = pid // pairs_per_row
+    pair_id = pid % pairs_per_row
 
-    g_id = tl.program_id(0)
-    row = g_id // groups_per_row
-    group_id = g_id % groups_per_row
-
-    y_ptr += row * y_row_stride + group_id * group_size
-    y_q_ptr += g_id * group_size
-    y_s_ptr += group_id * y_s_col_stride + row
-
+    group0 = pair_id * groups_per_program
+    g0 = row * groups_per_row + group0
+    base = y_ptr + row * y_row_stride
     cols = tl.arange(0, BLOCK)
     mask = cols < group_size
 
-    y = tl.load(y_ptr + cols, mask=mask, other=0.0).to(tl.float32)
-    _absmax = tl.maximum(tl.max(tl.abs(y)), eps)
-    y_s = _absmax / fp8_max
+    for i in tl.static_range(0,groups_per_program):
+        group = group0 + i
+        g = g0 + i
+        y_ptr_g = base + group * group_size
+        y_q_ptr_g = y_q_ptr + g * group_size
+        y_s_ptr_g = y_s_ptr + group * y_s_col_stride + row
+        y = tl.load(y_ptr_g + cols, mask=mask, other=0.0).to(tl.float32)
 
-    if scale_ue8m0:
-        y_s = tl.exp2(tl.ceil(tl.log2(tl.maximum(tl.abs(y_s), 1e-10))))
+        abs_g = tl.abs(y)
+        max_g = tl.max(abs_g)
 
-    y_q = tl.clamp(y / y_s, fp8_min, fp8_max).to(y_q_ptr.dtype.element_ty)
+        y_s = tl.maximum(max_g, eps) / fp8_max
 
-    tl.store(y_q_ptr + cols, y_q, mask=mask)
-    tl.store(y_s_ptr, y_s)
+        if scale_ue8m0:
+            y_s = tl.exp2(tl.ceil(tl.log2(tl.maximum(tl.abs(y_s), 1e-10))))
 
+        y_q = tl.clamp(y / y_s, fp8_min, fp8_max).to(y_q_ptr.dtype.element_ty)
+    
+        tl.store(y_q_ptr_g + cols, y_q, mask=mask)
+        tl.store(y_s_ptr_g, y_s)
+        
+    
 
 def per_token_group_quant_fp8(
     x: torch.Tensor,
@@ -133,8 +155,22 @@ def per_token_group_quant_fp8(
     BLOCK = triton.next_power_of_2(N)
     num_warps = min(max(BLOCK // 256, 1), 8)
     num_stages = 1
+    def Groups_per_program(x, group_size) -> int:
+        if (x.shape[-1] // group_size) % 8 == 0:
+            return 8
+        elif (x.shape[-1] // group_size) % 4 == 0:
+            return 4
+        elif (x.shape[-1] // group_size) % 2 == 0:
+            return 2
+        else:
+            return 1
+        
+    groups_per_program = Groups_per_program(x, group_size)
+    
+
+    # print("HIT per_token_group_quant_fp8")
     if column_major_scales:
-        _per_token_group_quant_fp8_colmajor[(M,)](
+        _per_token_group_quant_fp8_colmajor[(M // groups_per_program,)](
             x,
             x_q,
             x_s,
@@ -149,9 +185,10 @@ def per_token_group_quant_fp8(
             BLOCK=BLOCK,
             num_warps=num_warps,
             num_stages=num_stages,
+            groups_per_program=groups_per_program,
         )
-    else:
-        _per_token_group_quant_fp8[(M,)](
+    else: 
+        _per_token_group_quant_fp8[(M // groups_per_program,)](
             x,
             x_q,
             x_s,
@@ -165,6 +202,7 @@ def per_token_group_quant_fp8(
             BLOCK=BLOCK,
             num_warps=num_warps,
             num_stages=num_stages,
+            groups_per_program=groups_per_program,
         )
-
+       
     return x_q, x_s
