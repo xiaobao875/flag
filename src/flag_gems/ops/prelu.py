a11c87ca3f64d9b2d2c75ac6c95f94828dd0472f
Denis <34310801+Oslomayor@users.noreply.github.com>
2026-05-09 16:44:41 +0800

    [MTHREADS] Fix device type checks with flaggems.device (#2994)
    * [MTHREADS] Fix device type checks with flaggems.device
    
    Signed-off-by: Denis <zhentao.du@mthreads.com>
    
    * [MTHREADS] Fix device type checks with flaggems.device
    
    Replace hardcoded is_cuda checks with device.type == flag_gems.device
    Replace torch.cuda.synchronize() with torch_device_fn.synchronize()
    
    ---------
    
    Signed-off-by: Denis <zhentao.du@mthreads.com>

--- src/flag_gems/ops/prelu.py
+++ src/flag_gems/ops/prelu.py
@@ -5,6 +5,8 @@ import torch
 import triton
 import triton.language as tl
 
+import flag_gems
+
 logger = logging.getLogger(__name__)
 
 
@@ -48,8 +50,8 @@ def prelu(*args, **kwargs):
     if x is None or weight is None:
         raise ValueError("prelu expects (input, weight) as arguments.")
 
-    if not (x.is_cuda and weight.is_cuda):
-        raise AssertionError("Tensors must be CUDA tensors.")
+    if x.device.type != flag_gems.device or weight.device.type != flag_gems.device:
+        raise AssertionError(f"Tensors must be {flag_gems.device} tensors.")
 
     # Ensure dtype match
     if weight.dtype != x.dtype:
